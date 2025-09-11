fa2ca11b8c27f210d9adfecb7ffd93233c6fe1b8
bitch217 <aidanbear0817@gmail.com>
2025-09-11 10:19:40 -0600

    unfinished code

---
+++ life.py
@@ -0,0 +1,18 @@
+wake = input("Do you want to wake up today: ")
+if wake == "yes":
+    print("you woke up and realized that you are now imbued with the power of the time stone")
+if wake == "no":
+    print("you continue to slumber as you do you encounter a strange dream where you flying in a colorful space and see a strange man he seems to be familiar to you he has a sickly wide grin on his face he tells you to make everyone happy")
+if wake == "yes":
+    school = input("do you continue to school with your new found god like powers: ")
+    if school == "yes":
+        if wake == "yes":
+            print("you proceed to school even though you are not practically a god now and discover that the school has been infested with evil gloinks")
+            # dictator/partially evil route
+    if school == "no":
+        print("You rightfully decide to not to go to school and instead go to your local place of governance and evaporate the already aging politicians into dust by speeding up time by %10000000000000000 and declare yourself the supreme leader of what was your town but now is considered a hostile state because of you.")
+        print("The US government now considers you a hostile of great importance and decided to go to war with you to take you down. Multiple special forces operatives have broken into your home attempting to neutralize you")
+        if school == "no":
+            drastic = input("will you do a drastic measure to ensure your control ")
+            if drastic == "yes":
+                print("you in desperation use your powers over time to turn everyone in the continental united states of america into babies and because of that action you become the de facto president of the united states. the other leaders of the world worried ")
