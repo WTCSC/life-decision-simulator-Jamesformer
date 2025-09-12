68381c44540661e50d396f4ba3bd305c45cdfba0
bitch217 <aidanbear0817@gmail.com>
2025-09-12 10:11:35 -0600

    unfinished code uplode 2

--- life.py
+++ life.py
@@ -1,10 +1,12 @@
 wake = input("Do you want to wake up today: ")
 if wake == "yes":
     print("you woke up and realized that you are now imbued with the power of the time stone")
+    # true evil beginning
 if wake == "no":
     print("you continue to slumber as you do you encounter a strange dream where you flying in a colorful space and see a strange man he seems to be familiar to you he has a sickly wide grin on his face he tells you to make everyone happy")
 if wake == "yes":
     school = input("do you continue to school with your new found god like powers: ")
+    # hero/banished hero beginning
     if school == "yes":
         if wake == "yes":
             print("you proceed to school even though you are not practically a god now and discover that the school has been infested with evil gloinks")
@@ -15,4 +17,8 @@ if wake == "yes":
         if school == "no":
             drastic = input("will you do a drastic measure to ensure your control ")
             if drastic == "yes":
-                print("you in desperation use your powers over time to turn everyone in the continental united states of america into babies and because of that action you become the de facto president of the united states. the other leaders of the world worried ")
+                print("you in desperation use your powers over time to turn everyone in the continental united states of america into babies and because of that action you become the de facto president of the united states. the other leaders of the world worried and schedule a meeting with you and as soon as you arrive you are ambushed by the best special forces teams in the world")
+            if drastic == "no":
+                print("because of your inaction you were captured and somehow being held in a prototype containment cell that suppresses the powers of gods and are sentenced to be locked in that cell forever. Your path ends here you got the PARTIALLY EVIL ending")
+                if drastic == "yes":
+                        devolve = input("will you show them a taste of your power")
