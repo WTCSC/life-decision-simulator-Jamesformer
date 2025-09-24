0076dfc670538a76c44c98c465cb5238f55cc930
bitch217 <aidanbear0817@gmail.com>
2025-09-24 09:48:23 -0600

    added text for user to rerun code

--- life.py
+++ life.py
@@ -19,26 +19,26 @@ if wake == "yes":
             if drastic == "yes":
                 print("you in desperation use your powers over time to turn everyone in the continental united states of america into babies and because of that action you become the de facto president of the united states. the other leaders of the world worried and schedule a meeting with you and as soon as you arrive you are ambushed by the best special forces teams in the world")
             if drastic == "no":
-                print("because of your inaction you were captured and somehow being held in a prototype containment cell that suppresses the powers of gods and are sentenced to be locked in that cell forever. Your path ends here you got the PARTIALLY EVIL ending")
+                print("because of your inaction you were captured and somehow being held in a prototype containment cell that suppresses the powers of gods and are sentenced to be locked in that cell forever. Your path ends here you got the PARTIALLY EVIL ending. Run again to get a different ending")
             elif drastic == "yes":
                      devolve = input("will you show them a taste of your power: ")
                      if devolve == "yes":
                          print("you devolve the leaders and spec ops minds into those of neanderthals and since their are no more political opponents to object to your overwhelming power except the people. They are actively organizing coup de'tat's against your control.")
                      if devolve == "no":
-                            print("you are filled with enough lead to block a small nuclear bombs worth of radiation you have been killed by the spec ops teams. you got the ALT PARTIAL EVIL ending")
+                            print("you are filled with enough lead to block a small nuclear bombs worth of radiation you have been killed by the spec ops teams. you got the ALT PARTIAL EVIL ending. Run again to get a different ending")
                      elif devolve == "yes":
                                 suppress = input("will you forever suppress the people and ensure your grip across the world: ")
                                 if suppress == "yes":
-                                     print("you use your power to permanently devolve all technology and human knowledge to those of the stone age and you punish those who resist to your power. You establish an iron grip that doesn't relent for a 1000 years. You got the DICTATOR ending")
+                                     print("you use your power to permanently devolve all technology and human knowledge to those of the stone age and you punish those who resist to your power. You establish an iron grip that doesn't relent for a 1000 years. You got the DICTATOR ending. Run again to get a different ending")
                                 if suppress == "no":
-                                     print("you distance yourself from the public eye and let the world burn with no leaders left. You got the BURNED WORLD ending")
+                                     print("you distance yourself from the public eye and let the world burn with no leaders left. You got the BURNED WORLD ending. Run again to get a different ending")
     # imprisoned hero/hero/fairly pathetic/government
     elif school == "yes":
              savior = input("will you us your powers for good: ")
              if savior == "yes":
                   print("you use your amazing powers to turn the gloinks into their docile version. You are hailed a hero for your actions but their are people calling your actions cruel. ")
              if savior == "no":
-                  print("you cower at the sight of the gloinks and hide from them. you are found and torn limb from limb. You got the FAIRLY PATHETIC ending")
+                  print("you cower at the sight of the gloinks and hide from them. you are found and torn limb from limb. You got the FAIRLY PATHETIC ending. Run again to get a different ending")
              elif savior == "yes":
                   address = input("will you address these concerns: ")
                   if address == "yes":
@@ -48,15 +48,15 @@ if wake == "yes":
                   elif address == "yes":
                        imprisoned = input("do you join the government: ")
                        if imprisoned == "yes":
-                            print("you join the department of defense and are a puppet of the government with no real free will every action is monitored. You are effectively a slave to the USA. you got the GOVERNMENT ending")
+                            print("you join the department of defense and are a puppet of the government with no real free will every action is monitored. You are effectively a slave to the USA. you got the GOVERNMENT ending. Run again to get a different ending")
                        if imprisoned == "no":
-                            print("because of your refusal you are detained and confined in a special cell that nullifies your powers. You got the IMPRISONED HERO ending")
+                            print("because of your refusal you are detained and confined in a special cell that nullifies your powers. You got the IMPRISONED HERO ending. Run again to get a different ending")
                   if address == "no":
                        hero = input("will you join the government: ")
                        if hero == "yes":
-                            print("you join the department of defense and are hailed a protector of american values and your likeness is used in government propaganda that is everywhere. You got the ALT GOVERNMENT ending")
+                            print("you join the department of defense and are hailed a protector of american values and your likeness is used in government propaganda that is everywhere. You got the ALT GOVERNMENT ending. Run again to get a different ending")
                        if hero == "no":
-                            print("you decline the governments offer and decide to become a traveling vigilante for solving areas of conflict across the world. You got the HERO ending.")
+                            print("you decline the governments offer and decide to become a traveling vigilante for solving areas of conflict across the world. You got the HERO ending. Run again to get a different ending")
 if wake == "no":
      Nochoice = input("do you follow the mans advice: ")
      if Nochoice == "yes" or Nochoice == "no":
@@ -69,4 +69,4 @@ if wake == "no":
                  print("you go into a deep slumber and you see the same man again he enchants you with his aura you wake up and feel the happiest you ever have. you remember the substance you were exposed to weeks ago you collect what remains of the substance")
                  spread = input("do you help the man make more people happy: ")
                  if spread == "yes" or spread == "no": 
-                         print("you gather enough of the substance to help multiple people be happy. you ascend into the shadows and hit multiple people with the spores and you feel relived that they will be embraced by the man. you contenue this for a full week later on into the week your memory starts to fade the only thing you remember is the man. Shortly after the weeks end you suddenly collapse dead after the fungus has ravaged your body. you got the TRUE EVIL ending")
+                         print("you gather enough of the substance to help multiple people be happy. you ascend into the shadows and hit multiple people with the spores and you feel relived that they will be embraced by the man. you continue this for a full week later on into the week your memory starts to fade the only thing you remember is the man. Shortly after the weeks end you suddenly collapse dead after the fungus has ravaged your body. you got the TRUE EVIL ending. Run again to get a different ending")
